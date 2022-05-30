68674cab7aaf1a5d216f5f99334c96c15a25f498
mohammadhaimour <mohammadhaimour1999@gmail.com>
2022-05-31 00:29:15 +0300

    solved class01

---
+++ handlers/404.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (req, res, next) => {
+    res.status(404).send(
+        {
+            code: 404,
+            route: req.path,
+            message: "page not found"
+        }
+    );
+
+}
